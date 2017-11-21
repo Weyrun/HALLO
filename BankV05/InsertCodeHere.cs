87508107660dd6107ab4a8c1bcdc5ccc26aceeb2
Mikkel <Mikk793a@edu.easj.dk>
2017-11-21 12:53:05 +0100

    hej

---
+++ BankV05/InsertCodeHere.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BankV05
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            BankAccount theAccount = new BankAccount();
+
+            theAccount.Deposit(2000);
+            Console.WriteLine($"Account balance is : {theAccount.Balance}");
+
+            theAccount.Withdraw(1500);
+            Console.WriteLine($"Account balance is : {theAccount.Balance}");
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
